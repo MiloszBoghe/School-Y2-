781e23331e467976ed417139c2972cfcbe9e0846
MiloszBoghePXL <11800460@student.pxl.be>
2019-12-11 15:09:13 +0100

    software analysis cleanup

---
+++ Net Advanced/Repos/H14/EFDEMO/EFDEMO.Data/Migrations/20191210140029_column_added.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFDEMO.Data.Migrations
+{
+    public partial class column_added : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TestProperty",
+                table: "Battles",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TestProperty",
+                table: "Battles");
+        }
+    }
+}
