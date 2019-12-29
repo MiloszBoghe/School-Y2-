77ea629e7014437583564e04ad8ba42759dffc27
Satan <--global>
2019-12-29 13:53:35 +0100

    ok

---
+++ Net Advanced/Repos/H14/EFDemo2/EFDemo.UI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFDemo.Data;
+using EFDemo.Domain;
+
+namespace EFDemo.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InsertSamurai();
+
+
+
+
+            Console.Write("\nPress return to quit...");
+            Console.ReadLine();
+        }
+
+        private static void InsertSamurai()
+        {
+            Samurai samurai = new Samurai
+            {
+                Name = "Samurai1",
+            };
+
+            using (SamuraiContext samuraiContext = new SamuraiContext())
+            {
+                samuraiContext.Samurais.Add(samurai);
+                samuraiContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Net Advanced/Repos/H14/SamuraiApp/SamuraiApp.Data/Migrations/20181130151326_BattleData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiApp.Data.Migrations
+{
+    public partial class BattleData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Battles",
+                columns: new[] { "Id", "EndDate", "Name", "StartDate" },
+                values: new object[] { 1, new DateTime(902, 2, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "The great battle", new DateTime(902, 1, 30, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+
+            migrationBuilder.InsertData(
+                table: "Battles",
+                columns: new[] { "Id", "EndDate", "Name", "StartDate" },
+                values: new object[] { 2, new DateTime(904, 10, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "The long battle", new DateTime(903, 10, 10, 0, 0, 0, 0, DateTimeKind.Unspecified) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Battles",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Battles",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
