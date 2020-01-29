fbee7edd8dd9b9be490debf6f22bf8455d774e3a
MiloszBoghePXL <11800460@student.pxl.be>
2020-01-29 21:25:13 +0100

    auto

---
+++ Net Advanced/ProefExamen/BankApplication/Bank.Datalayer/Migrations/20200129163803_fx.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bank.Datalayer.Migrations
+{
+    public partial class fx : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cellphone",
+                table: "Customers",
+                newName: "CellPhone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CellPhone",
+                table: "Customers",
+                newName: "Cellphone");
+        }
+    }
+}
