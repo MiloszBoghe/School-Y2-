fa6233aaf6b427d640c6ce1996ee8de02a807f40
Satan <11800460@student.pxl.be>
2020-01-29 22:36:15 +0100

    auto

---
+++ Net Advanced/Repos/SamuraiApp/SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SamuraiAppData;Integrated Security=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(sb => new {sb.SamuraiId, sb.BattleId});
+
+            modelBuilder.Entity<Battle>().HasData(
+                new Battle
+                {
+                    Id = 1,
+                    Name = "The great battle",
+                    StartDate = new DateTime(902, 1, 30),
+                    EndDate = new DateTime(902, 2, 10)
+                },
+                new Battle
+                {
+                    Id = 2,
+                    Name = "The long battle",
+                    StartDate = new DateTime(903, 10, 10),
+                    EndDate = new DateTime(904, 10, 10)
+                });
+        }
+    }
+}
---
+++ Net Advanced/Repos/SamuraiApp/SamuraiApp.UI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SamuraiApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
