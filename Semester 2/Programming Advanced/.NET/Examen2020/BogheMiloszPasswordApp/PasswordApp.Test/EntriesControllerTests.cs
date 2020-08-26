35c339c0139cef65396bf7a0604cdbb32cf37a5d
MiloszBoghePXL <11800460@student.pxl.be>
2020-08-26 15:45:31 +0200

    auto

--- Semester 2/Programming Advanced/.NET/Examen2020/BogheMiloszPasswordApp/PasswordApp.Test/EntriesControllerTests.cs
+++ Semester 2/Programming Advanced/.NET/Examen2020/BogheMiloszPasswordApp/PasswordApp.Test/EntriesControllerTests.cs
@@ -8,7 +8,6 @@ using PasswordApp.Web.Controllers;
 using PasswordApp.Web.Models;
 using PasswordApp.Web.Services.Contracts;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
